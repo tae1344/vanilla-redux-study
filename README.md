4e698e4fb87a453adb477cbc6ba3487f94f495ac
김태연 <xodus1344.hello@gmail.com>
2021-04-14 15:30:33 +0900

    Vanilla redux counter example

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Vanilla Redux
 
 Learning Vanilla-Redux and React-Redux
+
+### Redux
+
+store : createStore(reducer Func)를 통해 '상태(state)'를 저장하는 저장소를 생성. dispatch, getState, replaceReducer, subscribe 4가지의 내장함수를 가지고 있다.
+reducer : store의 state 데이터를 변경, 수정해 새로운 값을 리턴하게 하는 '함수'다.
+action :
--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// 리듀서 --> 앱의 데이터를 수정하고 변경해 새로운 값을 리턴하는 '함수'다.
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  count--;
-  updateText();
+const countStore = createStore(countModifier); // 앱의 데이터를 저장하는 역할
+
+// subscribe에 listener로 전달할 함수
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+countStore.subscribe(onChange); // store 안의 상태 변화를 구독(감지)한다.
+
+plus.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
