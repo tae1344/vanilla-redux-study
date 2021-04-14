7b811567f55a2ee04716a4b718182f298782ea4e
김태연 <xodus1344.hello@gmail.com>
2021-04-14 14:14:29 +0900

    vanilla JS simple app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let count = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count++;
+  updateText();
+};
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+plus.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
